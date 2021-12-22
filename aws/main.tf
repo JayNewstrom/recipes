b8feda20c53be4fd6a8727a9bbdf12c18e71f1e1
Jay Newstrom <jaynewstrom@gmail.com>
2021-12-22 14:45:32 -0600

    Be more restrictive on bucket settings.

--- aws/main.tf
+++ aws/main.tf
@@ -55,6 +55,15 @@ resource "aws_s3_bucket" "s3_bucket" {
   tags   = var.aws_tags
 }
 
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_cloudfront_distribution" "s3_distribution" {
   depends_on = [
     aws_s3_bucket.s3_bucket
